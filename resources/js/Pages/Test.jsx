121592f92729a815cc25146622c8ce119147cc0b
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-06-10 12:20:44 -0500

    Ya funciona el Inertia y React
    Faltó mi compa el ziggy pero ya luego xd

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <p>Hola soi el miki</p>
+    )
+}
+
+export default HomePage;
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/inertia', function () {
+    return Inertia::render('Ejemplo');
 });
