86d8cf6ef7f7e0febd17264ac4009e950472ef73
PapaDrago <oscarwaii@hotmail.com>
2021-06-12 20:34:35 -0500

    inicio 50%
    al ratillo le sigo

--- resources/js/Pages/Inicio.jsx
+++ resources/js/Pages/Inicio.jsx
@@ -3,6 +3,10 @@ import Layout from '../layouts/Layout';
 import '/css/inicio.css';
 import Grid from '@material-ui/core/Grid';
 
+//componentes
+import Libro from '../components/Libro';
+import Club from '../components/Club';
+
 //Iconos
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
@@ -14,20 +18,20 @@ const Inicio = () => {
         <>
             <div style={{width:'100%',backgroundColor:'#000000'}}>
                 <div className="portadaInicio">
-                    <img src="/img/portadas/portadaInicio3.png"></img>
+                    <img src="/img/portadas/portada1.jpg"></img>
                 </div>
                 <Grid container direction="row" justify="center" alignItems="center" style={{marginTop:48,paddingBottom:150}}>
-                    <a href="" target="_blank" style={{marginLeft:12,marginRight:12}}><FacebookIcon    style={{color:'#FFFFFF'}}></FacebookIcon></a>
-                    <a href="" target="_blank" style={{marginLeft:12,marginRight:12}}><TwitterIcon     style={{color:'#FFFFFF'}}></TwitterIcon></a>
-                    <a href="" target="_blank" style={{marginLeft:12,marginRight:12}}><InstagramIcon   style={{color:'#FFFFFF'}}></InstagramIcon></a>
-                    <a href="" target="_blank" style={{marginLeft:12,marginRight:12}}><YouTubeIcon     style={{color:'#FFFFFF'}}></YouTubeIcon></a>
+                    <a href="https://www.facebook.com/YoSoyDanteEludier" target="_blank" style={{marginLeft:12,marginRight:12}}><FacebookIcon    style={{color:'#FFFFFF'}}></FacebookIcon></a>
+                    <a href="https://twitter.com/DANTEELUDIER" target="_blank" style={{marginLeft:12,marginRight:12}}><TwitterIcon     style={{color:'#FFFFFF'}}></TwitterIcon></a>
+                    <a href="https://www.instagram.com/danteeludier/" target="_blank" style={{marginLeft:12,marginRight:12}}><InstagramIcon   style={{color:'#FFFFFF'}}></InstagramIcon></a>
+                    <a href="https://www.youtube.com/channel/UC7XJQ4Vrl5dvv4TPW9KpHwQ" target="_blank" style={{marginLeft:12,marginRight:12}}><YouTubeIcon     style={{color:'#FFFFFF'}}></YouTubeIcon></a>
                 </Grid>
             </div>
             <Grid container justify="center" style={{backgroundColor:'#E5E5E5'}}>
                 <div className="inicio_rounded">
-                    <div style={{height:380}}>olña</div>
+                    <Libro />
+                    <Club />
                 </div>
-                <Grid item xs={12}>olas</Grid>
             </Grid>  
         </>
     )
