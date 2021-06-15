d7e38ed02d5ba98c97a6308bb104f30801a5d453
PapaDrago <oscarwaii@hotmail.com>
2021-06-14 21:02:58 -0500

    amos a conectar los seeders
    zy

--- resources/js/components/Clientes.jsx
+++ resources/js/components/Clientes.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+
 
 //CSS
 import '/css/inicio.css';
-import '/css/libros.css';
 
 //OWL
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 
 export default function Clientes(){
     const responsive = {
@@ -30,9 +32,12 @@ export default function Clientes(){
     }
     return (
         <>
+            <div className="clientes_title">Nuestros Clientes</div>
             <div className='container' style={{marginTop:30,marginBottom:60}}>            
-                <OwlCarousel responsive={responsive} className="owl-theme" loop dots={false} margin={20}>
-                    <div className="item clientes_item"><img src="/img/clientes/5.png"/></div>
+                <OwlCarousel responsive={responsive} className="owl-theme" loop dots={false} margin={20} autoplay autoplayTimeout={2000} autoplayHoverPause>
+                    <Tooltip title="Mary Kay" placement="top-start">
+                        <a href="https://www.marykay.com.mx/"  target="_blank" className="item clientes_item"><img src="/img/clientes/5.png"/></a>
+                    </Tooltip>
                     <div className="item clientes_item"><img src="/img/clientes/2.png"/></div>
                     <div className="item clientes_item"><img src="/img/clientes/3.png"/></div>
                     <div className="item clientes_item"><img src="/img/clientes/4-1.png"/></div>
