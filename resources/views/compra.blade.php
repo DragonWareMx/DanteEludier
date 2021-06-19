6bfad541f944ff6217414a4cc3ca47c943220f91
Dulce Gabriela Marín Rendón <dgmarin61@gmail.com>
2021-06-19 00:48:45 -0500

    jeje

--- resources/views/compra.blade.php
+++ resources/views/compra.blade.php
@@ -177,18 +177,6 @@
 
         <hr>
 
-        {{-- <div class="libro-regresar">
-            <div class="boton">
-                <button onclick="location.href='{{ route('inicio') }}'" type="button">
-                    <div class="row" style="margin-right:0px; margin-left: auto;">
-                        <img src="{{ asset('img/ico/blackarrow.png') }}">
-                    </div>
-                    <div class="row" style="margin-right:0px; margin-left: auto;">
-                        Regresar
-                    </div>
-                </button>
-            </div>
-        </div> --}}
     </form>
 
     <script>
