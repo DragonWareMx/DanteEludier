499f404cedf9e35d942294798338028f654c5c7f
PapaDrago <oscarwaii@hotmail.com>
2021-06-19 01:02:03 -0500

    ya merito

--- resources/views/emails/contactomail.blade.php
+++ resources/views/emails/contactomail.blade.php
@@ -115,6 +115,102 @@
   </div>
   <!-- end preheader -->
 
-</body>
+  <!-- start body -->
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
 
+    <!-- start logo -->
+    <tr>
+      <td align="center" bgcolor="#1a1a1a">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="center" valign="top" style="padding: 36px 24px;">
+              <a href="https://danteeludier.com/" target="_blank" style="display: inline-block;">
+                <img
+                  src="data:image/png;base64,{{base64_encode(file_get_contents(public_path('img/danteLogoBlanco.png')))}}"
+                  alt="Logo" border="0" width="250px"
+                  style="display: block; width: 250px; max-width: 250px; min-width: 48px; filter: opacity(1)">
+              </a>
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end logo -->
+
+    <!-- start hero -->
+    <tr>
+      <td align="center" bgcolor="#1a1a1a">
+        <!--[if (gte mso 9)|(IE)] 83D7B5>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+        <tr>
+        <td align="center" valign="top" width="600">
+        <![endif]-->
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="left" bgcolor="#FAFAFA"
+              style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #000000;">
+              <h1
+                style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;color: black">
+                ¡Nuevo mensaje!</h1>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" bgcolor="#1a1a1a">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="left" bgcolor="#FAFAFA"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <p style="margin: 0;">{{$request->nombre}} ha enviado un mensaje desde la página de contacto.></p>
+            </td>
+          </tr>
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              Correo electrónico: {{$request->email}}
+            </td>
+          </tr>
+          <tr>
+            <td align="left" bgcolor="#ffffff"
+              style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              Mensaje: {{$request->mensaje}}
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" bgcolor="#1a1a1a" style="padding: 24px;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+          <tr>
+            <td align="center" bgcolor="#1a1a1a"
+              style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;"></p>
+            </td>
+          </tr>
+          <tr>
+            <td align="center" bgcolor="#1a1a1a"
+              style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
+              <p style="margin: 0;">© 2020 Dante Eludier.</p>
+              <p style="margin: 0;">Todos los derechos reservados.</p>
+              <p style="margin: 0;">México.</p>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
 </html>
